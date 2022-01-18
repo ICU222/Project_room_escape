8de295a4d3575599c23379e5f0d3fee21694c096
KoJaeWon <jason2560@naver.com>
2022-01-18 15:34:18 +0900

    solve test code error : Annotate the crash package : .SpringbootTest

--- src/test/java/icu222/roomescape/web/RoomescapeApplicationTests.java
+++ src/test/java/icu222/roomescape/web/RoomescapeApplicationTests.java
@@ -3,26 +3,29 @@ package icu222.roomescape.web;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.context.SpringBootTest;
+// SpringBootTest와 .servlet.WebMvcTest은 서로 중복이 된다. issue 확인
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+//====================================================================
 
-@ContextConfiguration(classes = testController.class)
+//import org.springframework.test.web.servlet.ResultActions;
+//import org.springframework.test.context.junit4.SpringRunner;
+//import org.springframework.test.context.ContextConfiguration;
+//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+
+
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(controllers = testController.class)
-@SpringBootTest
+//@SpringBootTest 중복
 public class RoomescapeApplicationTests {
 	@Autowired
 	private MockMvc mvc;
