2b0e34b43b11e2d74b3ad0e8a45375f0016b1d39
KoJaeWon <jason2560@naver.com>
2022-01-19 15:30:24 +0900

    add json api test code in testControllerTest

--- src/test/java/icu222/roomescape/web/TestControllerTest.java
+++ src/test/java/icu222/roomescape/web/TestControllerTest.java
@@ -1,20 +1,23 @@
 package icu222.roomescape.web;
 
+import icu222.roomescape.web.dto.TestResponseDto;
 import org.junit.jupiter.api.Test;
+
+// 해당 패키지는 책의 .runner.Runwith 대신 사용
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 //import org.springframework.boot.test.context.SpringBootTest;
 // SpringBootTest와 .servlet.WebMvcTest은 서로 중복이 된다. issue 확인
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 //====================================================================
 
 //import org.springframework.test.web.servlet.ResultActions;
@@ -26,7 +29,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(controllers = testController.class)
 //@SpringBootTest 중복
-public class RoomescapeApplicationTests {
+public class TestControllerTest {
 	@Autowired
 	private MockMvc mvc;
 
@@ -37,6 +40,21 @@ public class RoomescapeApplicationTests {
 		mvc.perform(get("/test"))
 				.andExpect(status().isOk())
 				.andExpect(content().string(test));
+	}
+
+	@Test
+	public void lombokDto_isReturn() throws Exception {
+
+		String name = "test";
+		int amount = 1000;
+
+		mvc.perform(
+				get("/test/dto").param("name", name)
+						.param("amount", String.valueOf(amount)))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.name", is(name)))
+				.andExpect(jsonPath("$.amount", is(amount)));
+
 	}
 
 }
--- src/test/java/icu222/roomescape/web/testController.java
+++ src/test/java/icu222/roomescape/web/testController.java
@@ -1,7 +1,11 @@
 package icu222.roomescape.web;
 
+import icu222.roomescape.web.dto.TestResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 @RestController
 public class testController {
@@ -10,6 +14,13 @@ public class testController {
         return "test";
     }
 
+    @GetMapping("/test/dto")
+    public TestResponseDto testDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+        return new TestResponseDto(name, amount);
+    }
 }
+
+
+
 
 
