d4cea89dc618a8d3d0925dc2a790ddaa95eb3915
KoJaeWon <jason2560@naver.com>
2022-01-19 14:44:28 +0900

    add dto test package, add dto code

---
+++ src/main/java/icu222/roomescape/web/dto/testResponseDto.java
@@ -0,0 +1,12 @@
+package icu222.roomescape.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class testResponseDto {
+
+    private final String name;
+    private final int amount;
+}
