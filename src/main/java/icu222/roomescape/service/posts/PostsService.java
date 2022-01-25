dc749635014673ed4ed83028199a614533ffbe81
KoJaeWon <jason2560@naver.com>
2022-01-25 13:57:33 +0900

    add PostsService

---
+++ src/main/java/icu222/roomescape/service/posts/PostsService.java
@@ -0,0 +1,17 @@
+package icu222.roomescape.service.posts;
+
+import icu222.roomescape.domain.posts.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
