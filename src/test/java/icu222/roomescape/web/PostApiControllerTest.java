3ad2f955a1f78f696f9a7863a6e8614454f64005
KoJaeWon <jason2560@naver.com>
2022-02-14 15:58:09 +0900

    change error packages

---
+++ src/test/java/icu222/roomescape/web/PostApiControllerTest.java
@@ -0,0 +1,13 @@
+package icu222.roomescape.web;
+
+//import org.junit.After;
+//import org.junit.Test;
+//import org.junit.runner.Runwith;
+
+
+public class PostApiControllerTest {
+
+    // add later
+
+}
+
